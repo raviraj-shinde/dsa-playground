ab94fb5d8004b6976cfc49c40a13020d4371306e
Raviraj <ravishinde96k@gmail.com>
2025-08-20 23:32:39 +0530

    Level Order BFS

--- Binary-Tree-I/Preorder.java
+++ Binary-Tree-I/Preorder.java
@@ -1,71 +1,110 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Preorder {
 
-    static class Node {
-        int data;
-        Node left;
-        Node right;
+  static class Node {
 
-        public Node(){};
-        public Node(int data){
-            this.data = data;
-        }
+    int data;
+    Node left;
+    Node right;
 
-        public Node(int data, Node left, Node right) {
-            this.data = data;
-            this.left = left;
-            this.right = right;
-        }
+    public Node() {}
+
+    public Node(int data) {
+      this.data = data;
     }
 
-    static int idx = 0; // shared index across recursion
+    public Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
 
-    public static Node preOrderTree(int[] nums) {
-        if (idx >= nums.length || nums[idx] == -1) { // base case
-            idx++; // move index forward for parent call
-            return null;
-        }
+  static int idx = 0; // shared index across recursion
 
-        Node node = new Node(
-            nums[idx++],               // consume current index
-            preOrderTree(nums),        // build left subtree
-            preOrderTree(nums)         // build right subtree
-        );
-        return node;
+  public static Node preOrderTree(int[] nums) {
+    if (idx >= nums.length || nums[idx] == -1) { // base case
+      idx++; // move index forward for parent call
+      return null;
     }
 
-    public static void preOrderTraversal(Node root){
-        //base condition
-        if(root == null){
-            System.out.print("-1 ");
-            return;
-        }
-        //work
-        System.out.print("" + root.data + " ");
-        preOrderTraversal(root.left); 
-        preOrderTraversal(root.right);
-        
+    Node node = new Node(
+      nums[idx++], // consume current index
+      preOrderTree(nums), // build left subtree
+      preOrderTree(nums) // build right subtree
+    );
+    return node;
+  }
+
+  public static void preOrderTraversal(Node root) {
+    //base condition
+    if (root == null) {
+      System.out.print("-1 ");
+      return;
     }
+    //work
+    System.out.print("" + root.data + " ");
+    preOrderTraversal(root.left);
+    preOrderTraversal(root.right);
+  }
 
-    public static void inOrderTraversal(Node root){
-        //base condtion
-        if (root == null){
-            //System.out.print(" -1");
-            return;
-        }
-        //work
-        
-        inOrderTraversal(root.left); 
-        System.out.print(" " + root.data);
-        inOrderTraversal(root.right);
+  public static void inOrderTraversal(Node root) {
+    //base condtion
+    if (root == null) {
+      //System.out.print(" -1");
+      return;
+    }
+    //work
 
+    inOrderTraversal(root.left);
+    System.out.print(" " + root.data);
+    inOrderTraversal(root.right);
+  }
+
+  public static void postOrderTraversal(Node root) {
+    //base condition
+    if (root == null) return;
+
+    //work
+    postOrderTraversal(root.left);
+    postOrderTraversal(root.right);
+    System.out.print(" " + root.data);
+  }
+
+  //By BFS, for DFS - recursion (int Level)
+  public static void levelOrderTraversal(Node root) {
+    //offer-poll-peek()
+    Queue<Node> queue = new LinkedList<>();
+    queue.offer(root);
+    queue.offer(null);
+
+    while (!queue.isEmpty()) {
+      if(queue.peek() != null){
+        Node node = queue.poll();
+        System.out.print(" " + node.data);
+        if(node.left != null) queue.offer(node.left);
+        if(node.right != null) queue.offer(node.right);
+      } else {
+          queue.poll();
+          if (queue.isEmpty()) return;
+          queue.offer(null);
+      }
     } 
-    public static void main(String[] args) {
-        int[] tree = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        idx = 0; // reset before building
-        Node root = preOrderTree(tree);
-        System.out.println(root.data); // should print 1
-        preOrderTraversal(root);  
-        System.out.println();
-        inOrderTraversal(root);  
-    }
+  }
+
+  public static void main(String[] args) {
+    int[] tree = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+    idx = 0; // reset before building
+    Node root = preOrderTree(tree);
+    System.out.println(root.data); // should print 1
+    preOrderTraversal(root);
+    System.out.println();
+    inOrderTraversal(root);
+    System.out.println();
+    postOrderTraversal(root);
+    System.out.println();
+    levelOrderTraversal(root);
+  }
 }
